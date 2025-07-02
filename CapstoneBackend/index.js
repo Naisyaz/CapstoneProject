00bbb51742375d7479dc430f70e948c3dd785501
Nurul Aisyah <nurulaisyahazman99@gmail.com>
2025-07-02 15:25:52 +0800

    Connecting frontend to backend

--- CapstoneBackend/index.js
+++ CapstoneBackend/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
-  const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const cors = require("cors")
   
-  const authRoutes = require("./routes/auth");
-  const taskRoutes = require("./routes/tasks");
+const authRoutes = require("./routes/auth");
+const taskRoutes = require("./routes/tasks");
   
   mongoose.connect("mongodb+srv://naisyaz:1234@cluster0.llb16i0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     const app = express();
     // From my frontend (revisit to reconnect)
-    //app.use(cors({
-    //    origin: 
-    //}))
+    app.use(cors({
+        origin: 'http://localhost:5173/',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTION'],
+        credentials: true,
+    }))
 
     app.use(express.json());
   

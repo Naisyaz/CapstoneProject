966c1f041a51f552da1527a131ac7bb572bc6536
Nurul Aisyah <nurulaisyahazman99@gmail.com>
2025-07-03 23:05:16 +0800

    Revert "Reapply "Update auth.js (changed secret)""
    This reverts commit 45cf62eb71072d8354ad042bcc6edfc69a58bd94.

--- CapstoneBackend/middleware/auth.js
+++ CapstoneBackend/middleware/auth.js
@@ -4,8 +4,8 @@ const jwt = require("jsonwebtoken");
     const authHeader = req.headers.authorization;
     if (!authHeader) return res.sendStatus(401);
     const token = authHeader.split(" ")[1];
-  // [changed the secret]
-    jwt.verify(token, "i6SqmayC6RnbvfNdcrDvmuzVznyHYi5wMyum3Ku21W4eGdjsdZaFqBcDvXltgHZGiUc01fEKzW0POgPEqRXlXg==", (err, decoded) => {
+  
+    jwt.verify(token, "secret", (err, decoded) => {
       if (err) return res.sendStatus(403);
       req.user = decoded;
       next();

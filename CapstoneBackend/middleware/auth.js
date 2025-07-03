56df78d946d5da9f6d257f824d0bb798507fd782
Nurul Aisyah <nurulaisyahazman99@gmail.com>
2025-07-03 22:55:10 +0800

    Update auth.js (changed secret)

--- CapstoneBackend/middleware/auth.js
+++ CapstoneBackend/middleware/auth.js
@@ -4,8 +4,8 @@ const jwt = require("jsonwebtoken");
     const authHeader = req.headers.authorization;
     if (!authHeader) return res.sendStatus(401);
     const token = authHeader.split(" ")[1];
-  
-    jwt.verify(token, "secret", (err, decoded) => {
+  // [changed the secret]
+    jwt.verify(token, "i6SqmayC6RnbvfNdcrDvmuzVznyHYi5wMyum3Ku21W4eGdjsdZaFqBcDvXltgHZGiUc01fEKzW0POgPEqRXlXg==", (err, decoded) => {
       if (err) return res.sendStatus(403);
       req.user = decoded;
       next();
